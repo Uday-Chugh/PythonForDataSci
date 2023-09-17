f82bc9ec57b41481847d1017547117cf31e41579
Uday-Chugh <144380141+Uday-Chugh@users.noreply.github.com>
2023-09-17 23:53:25 +0530

    Create Q11.py

---
+++ Assignment2/Q11.py
@@ -0,0 +1,9 @@
+list1 = ["Hello ", "take "]
+list2 = ["Dear", "Sir"]
+list3=[]
+for a in range(0,2):
+    for b in range(0,2):
+        list3.append(list1[a]+list2[b])
+print(list3)
+
+# nested loop 
